e4e446c74ea389f4540a071b12f288730b2b94d1
Anthony A. Tirabassi (T) <143787394+t-tirabassi@users.noreply.github.com>
2024-01-30 17:22:12 -1000

    Implemented sum functions

---
+++ sumofnumbers.js
@@ -0,0 +1,40 @@
+const numbers = [1, 2, 3, 4, 5];
+
+function sumFor(list) {
+  let sum = 0;
+  for (let i = 0; i < list.length; ++i) {
+    sum += list[i];
+  }
+
+  return sum;
+}
+
+console.log(sumFor(numbers));
+
+function sumWhile(list) {
+  let sum = 0;
+  let i = 0;
+  while (i < list.length) {
+    sum += list[i];
+    ++i;
+  }
+
+  return sum;
+}
+
+console.log(sumWhile(numbers));
+
+function sumRecursionWay(list, index) {
+  if (index === list.length) {
+    return 0;
+  }
+  return list[index] + sumRecursionWay(list, index + 1);
+}
+
+console.log(sumRecursionWay(numbers));
+
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(numbers));
